c814acbca2b7a7c67480af8e9cd1f8c4a46ec100
trasigor <trasigor@users.noreply.github.com>
2019-12-24 18:55:35 +0100

    Removed unnecessary tag; style improvement

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,17 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" class="bg-blue">
-      <router-link to="/" class="navbar-brand p-0"><img class="logo" src="./../assets/images/logo_white.png"></router-link>
+  <b-navbar toggleable="md" type="dark" class="bg-blue">
+    <router-link to="/" class="navbar-brand p-0"><img class="logo" src="./../assets/images/logo_white.png"></router-link>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto" right>
-          <router-link to="/debt" class="nav-item nav-link">Minhas Dívidas</router-link>
-          <router-link to="/" class="nav-item nav-link">Meus Acordos</router-link>
-          <router-link to="/contact" class="nav-item nav-link">André Oliveira</router-link>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto" right>
+        <router-link to="/debt" class="nav-item nav-link">Minhas Dívidas</router-link>
+        <router-link to="/" class="nav-item nav-link">Meus Acordos</router-link>
+        <router-link to="/contact" class="nav-item nav-link">André Oliveira</router-link>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
