77811de9fcfefc8146d97575928bf164e309fdae
trasigor <trasigor@users.noreply.github.com>
2019-12-24 18:44:36 +0100

    Navbar collapse fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,19 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-blue">
-    <router-link to="/" class="navbar-brand p-0"><img class="logo" src="./../assets/images/logo_white.png"></router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ml-auto">
-        <router-link to="/debt" class="nav-item nav-link">Minhas Dívidas</router-link>
-        <router-link to="/" class="nav-item nav-link">Meus Acordos</router-link>
-        <router-link to="/contact" class="nav-item nav-link">André Oliveira</router-link>
-      </div>
-    </div>
-  </nav>
+  <div>
+    <b-navbar toggleable="lg" type="dark" class="bg-blue">
+      <router-link to="/" class="navbar-brand p-0"><img class="logo" src="./../assets/images/logo_white.png"></router-link>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto" right>
+          <router-link to="/debt" class="nav-item nav-link">Minhas Dívidas</router-link>
+          <router-link to="/" class="nav-item nav-link">Meus Acordos</router-link>
+          <router-link to="/contact" class="nav-item nav-link">André Oliveira</router-link>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
